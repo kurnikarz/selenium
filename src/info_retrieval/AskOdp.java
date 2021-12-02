0eea61954ad591e1307225dc0277ee7a8b4838bc
Artur Dymkowski <arturdymkowski@wp.pl>
2021-12-02 10:50:03 +0100

    przepisy do ask

--- src/info_retrieval/AskOdp.java
+++ src/info_retrieval/AskOdp.java
@@ -11,83 +11,67 @@ public class AskOdp {
     public AskOdp(int index) {
         List<String> odpowiedzi = new ArrayList<>();
 
-        odpowiedzi.add("Ciasto z mrożonymi owocami: " +
+        odpowiedzi.add("Zapiekanka" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "    Opakowania makaronu (u mnie kokardki)\n" +
+                "    Ser (żółty mozzarella)\n" +
+                "    Pomidor\n" +
+                "    Szynka\n" +
+                "    Zioła np tymianek\n" +
+                "    1 łyżka masła\n" +
+                "    2 łyżki mąki\n" +
+                "    2 szklanki mleka\n" +
+                "    1 szczypta gałki muszkatołowej\n" +
+                "    1 szczypta kurkumy sól pieprz\n" +
+                "\n" +
                 "" +
-                "\n" +
-                "\n" +
-                "    5 jajek\n" +
-                "    3/4 - 1 szklanki cukru(zależy jak słodkie chcesz ciasto)\n" +
-                "    2 szklanki mąki\n" +
-                "    1/2 szklanki oleju\n" +
-                "    2 łyżeczki proszku do pieczenia\n" +
-                "    2 łyżki ciemnego kakao\n" +
-                "    Mrożone owoce w/g uznania\n" +
-                "    bułka tarta do obsypania owocow\n" +
-                "\n" +
-                "1. Mrożone owoce obsypujemy cukrem i bułką tartą." +
-                "2. Jajka o temperaturze pokojowej miksujemy z cukrem." +
-                "3. Nadal miksując dolewamy cienkim strumieniem olej." +
-                "4. Zwalniamy obroty i dodajemy mąkę z proszkiem do pieczenia." +
-                "5. Około 1/2 ciasta odlewamy do innego naczynia, do pozostałej część dosypujemy kakao i miksujemy do połączenia się składników." +
-                "6. Do wyłożonej papierem do pieczenia formy wykładamy ciasto ciemne i po wyrównaniu powierzchni wylewamy na nie ciasto jasne." +
-                "7. Na ciasto wykładamy zamrożone, przemieszane z cukrem i bułką tartą owoce." +
-                "8. Wstawiamy do piekarnika o temperaturze 175-180 stopni na około 45 minut." +
-                "9. Ciasto jest upieczone gdy wbity w nie patyczek jest po wyjęciu suchy." +
-                "10. \n" +
-                "\n" +
-                "Po wyjęciu z piekarnika obsypujemy wierzch ciasta cukrem pudrem. Smacznego! ");
+                "1. Makaron gotujemy w osolonej wodzie przelewamy na sito płuczemy. Szynkę i pozostałe składniki kroimy w kostkę. Masło roztopić w garnku. Dodać mąkę wymieszać i gdy tylko połączy się z masłem dodać mleko. Mieszamy energicznie trzepaczką by nie zrobiły się grudki. Gotować chwilę aby sos zgęstniał. Doprawić sól pieprz kurkumą i gałką muszkatołową do smaku (nie wolno z nią przesadzić bo sos będzie nie dobry.)" +
+                "2. Makaron mieszamy z pokrojonymi składnikami i częścią startego sera. Przekładamy do naczynia żaroodpornego zalewamy sosem beszamelowy posypujemy resztą sera i kładziemy pokrojone w plastry ser mozarella. Piec 180 st około 40 min.\n" +
+                "Posypujemy ziołami. SMACZNEGO!");
 
 
-        // ================================================================================
+        // ==================================================================================
 
-        odpowiedzi.add("Wiśniowe bajaderki " +
-                "\n" +
-                "    500 g resztek ciast\n" +
-                "    1 szklanka parzonych rodzynek\n" +
-                "    1/2 szklanki orzeszków ziemnych\n" +
-                "    100 g cukru\n" +
-                "    150 ml mleka\n" +
-                "    75 g masła\n" +
-                "    1 łyżka nutelli\n" +
-                "    1 łyżka masła orzechowego\n" +
-                "    2 łyżki dżemu wiśniowego\n" +
-                "    3 łyżki mielonego maku\n" +
-                "    4 łyżki wiórków kokosowych\n" +
-                "    Wiórki kokosowe do obtoczenia\n" +
-                "\n" +
-                        "1. Mleko, masło i cukier zagotuj. Następnie ostudź." +
-                        "2. Wszystkie składniki umieść w malakserze i zmiksuj na gładką masę. Następnie schowaj do lodówki na 2 godziny." +
-                        "3. Z ok. 1 łyżki masy uformuj kulkę i obtocz w wiórkach kokosowych. Gotowe."
-                );
-
-
-        // ================================================================================
-
-
-        odpowiedzi.add("Cynamonowe racuchy na jogurcie i śmietanie " +
-                "" +
+        odpowiedzi.add("Rolada dwa mięsa " +
                 "\n" +
                 "\n" +
-                "    100 g mąki pszennej\n" +
-                "    1 jajko\n" +
-                "    70 g śmietany 18 %\n" +
-                "    180 g jogurtu naturalnego gęstego\n" +
-                "    1 szczypta soli\n" +
-                "    25 g oliwy z oliwek\n" +
-                "    50 g cukru\n" +
-                "    1 łyżeczka sody oczyszczonej\n" +
+                "    2 bardzo duże filety z kurczaka\n" +
+                "    300 g karkówki\n" +
+                "    1 opakowania ciasta filo\n" +
+                "    5 łyżek musztardy dijon\n" +
+                "    5 żółtek\n" +
+                "    3 łyżki roztopionego masła\n" +
+                "    Sól, pieprz, zioła prowansalskie\n" +
+                "\n" +
+                "1. Filety podzielic na kotlety i cienko rozbić. Oprószyć z jednej strony solą i pieprzem. Karkówkę pokroić w paski również przyprawić solą i pieprzem. Z drugiej strony filet posmarować musztardą i poukładać karkówkę tak do połowy fileta. Zwinąć roladę." +
+                "2. Płat ciasta posmarować roztrzepanym żółtkiem. Na brzegu ciasta ułożyć roladkę założyć brzegi do środka i zawinąć roladkę (taki większy gołąbek)." +
+                "3. Wyłożyć na blaszkę do pieczenia, posmarować roztopionym masłem i posypać ziołami prowansalskimi. Piec w piekarniku nagrzanym do 180°C 40-50 min. Wyłączyć piekarnik i zostawić jeszcze na 20 min.");
+
+
+
+        // ==================================================================================
+
+        odpowiedzi.add("Bananowiec" +
+                "\n" +
+                "\n" +
+                "    5 bananów (bardzo dojrzałe)\n" +
+                "    2 szklanki mąki pszennej\n" +
                 "    1 łyżeczka proszku do pieczenia\n" +
-                "    1 łyżeczka cynamonu\n" +
-                "    olej do smażenia\n" +
-                "    dżem / marmolada / domowa nutella / cukier puder\n" +
+                "    1 łyżeczka sody oczyszczonej\n" +
+                "    2 jaja\n" +
+                "    120 gr miękkiego masła\n" +
+                "    8 łyżek mleka ciepłego\n" +
+                "    3/4 szklanki cukru\n" +
                 "\n" +
-                "1. Mąkę, cukier, sól, cynamon, sodę oczyszczoną i proszek do pieczenia, wymieszać." +
-                "2. Wlać jogurt naturalny, śmietanę 18 %, oliwę z oliwek i jajko, dokładnie wymieszać." +
-                "3. Smażyć na rozgrzanym oleju (niezbyt mocnym, aby zdążyły się upiec w środku)." +
-                "4. Podawać z tym na co mamy ochotę.");
+                "1. Banana rozgnieść widelcem na papkę. Mąkę wymieszać i przesiać z proszkiem i sodą. Masło utrzeć z cukrem. Miksując dodać po jednym jajku. Następnie dodawać naprzemiennie: mąkę, mleko, banana. Masa nie powinna być gęsta, jednak tak może się zdarzyć, wówczas wszystko wymieszać łyżką." +
+                "2. Foremki keksówki (dwie małe) wysmarować masłem i wlać ciasto. Piekarnik nagrzać do 180stopni i piec około 50 minut. Do suchego patyczka." +
+                "3. Ciasto pozostawić do wystygnięcia w piekarniku.");
 
 
-        // ================================================================================
+        // ==================================================================================
+
 
         odpowiedzi.add("Makaron z duszonymi warzywami z kalarepką " +
                 "" +
@@ -107,73 +91,14 @@ public class AskOdp {
                 "    Pieprz\n" +
                 "    Natka pietruszki\n" +
                 "\n" +
+                "" +
                 "1. Warzywa umyj i pokrój w kosteczkę, liście i łodygi kalarepki rozdrobnij" +
                 "2. Wstaw wodę na makaron, wrzuć do osolonej wody pokrojoną kalarepkę (korzeń)" +
                 "3. Na oliwie podsmaż paprykę cukinię cebulę i czosnek. Dodaj też pokrojone liście i łodygi kalarepki. Duś pod przykryciem. W razie potrzeby dodaj wody. Przypraw do smaku." +
                 "4. Po 10 minutach do gotującej się kalarepki wrzuć makaron i gotuj jeszcze około 10 minut zgodnie z przepisem na opakowaniu" +
-                "5. Odcedź makaron i kalarepkę. Wyłóż wszystko na talerz. Posyp natką pietruszki i bazylią. Smacznego");
-
-
-        // ================================================================================
-
-
-        odpowiedzi.add("Sałatka Grecka-Niemiecka  " +
+                "5. \n" +
                 "\n" +
-                "\n" +
-                "\n" +
-                "    200 g białych szparagów\n" +
-                "    200 g zielonych szparagów\n" +
-                "    1 czerwona papryka\n" +
-                "    1 mały zielony ogórek\n" +
-                "    1 cebula\n" +
-                "    200 g pomidorków koktajlowych\n" +
-                "    15 czarnych oliwek\n" +
-                "    1 łyżka kaparów\n" +
-                "    ewentualnie 150 g sera halummi\n" +
-                "    2 łyżki posiekanej natki pietruszki lub kolendry\n" +
-                "    5 łyżek oliwy z oliwek\n" +
-                "    sok z połówki cytryny\n" +
-                "    Sól i pieprz\n" +
-                "\n" +
-                "1. Szparagi myjemy, białe obieramy, z zielonych odłamujemy tylko zdrewniałą część\n" +
-                "Kroimy na około 3 cm kawałki i gotujemy w osolonej wodzie - najpierw białe około 5 minut, po czym dodajemy zielone i gotujemy je przez kolejne 3 minuty. Odcedzamy i przelewamy zimną wodą dla zahartowania." +
-                "2. Paprykę oczyszczamy z gniazd nasiennych i kroimy w większą kostkę. Podpiekamy na suchej patelni przez chwilę do zarumienienia. Cebulę obieramy, kroimy w pióra. Ogórka myjemy, kroimy na pół, wydrążamy środek i kroimy w grubszą kostkę. Pomidorki myjemy i przekrawamy w ćwiartki. Oliwki kroimy na połówki. Jeżeli wykorzystujemy ser to również kroimy go grubszą kostkę." +
-                "3. Przestudzone szparagi, paprykę, pozostałe warzywa mieszamy ze sobą dodajemy kapary, posiekaną pietruszkę, sok z cytryny, oliwę i doprawiamy dla smaku solą i pieprzem. Można podawać ze świeżą bagietką. Smacznego!");
-
-
-        // ================================================================================
-
-
-        odpowiedzi.add("Sałatka na kilka kęsów " +
-                "\n" +
-                "sos :\n" +
-                "1 łyżeczka musztardy chrzanowej\n" +
-                "4 łyżki oleju lnianego\n" +
-                "1 łyżeczka octu\n" +
-                "do smaku sól\n" +
-                "do smaku słodka papryka\n" +
-                "do smaku pieprz\n" +
-                "1 łyżeczka syropu trzcinowego\n" +
-                "składniki sałatki :\n" +
-                "1 garść sałaty rzymskiej (młode liście)\n" +
-                "1 mała garść świeżego szpinaku\n" +
-                "1 jajko (ugotowane na twardo)\n" +
-                "1/4 pomidora\n" +
-                "1 plaster cebuli\n" +
-                "1 truskawka\n" +
-                "2 plastry szynki lub innej wędliny\n" +
-                "1 łyżka sera sałatkowego typu feta\n" +
-                "2 łyżki grzanek czosnkowych\n" +
-                "1/2 łyżeczki czarnuszki\n" +
-                "kilka szczypiorków\n" +
-                "1 mała gałązka natki pietruszki\n" +
-                "" +
-                "1. Liście sałaty i szpinaku umyć i dokładnie osuszyć.\n" +
-                "Rozłożyć w misce i przygotować pozostałe składniki.\n" +
-                "Jajko pokroić na ćwiartki, pomidora na cząstki, wędlinę pokroić na pół i zwinąć w rożki, cebulę pokroić na cienkie paseczki, truskawkę na ćwiartki.\n" +
-                "Składniki sosu wymieszać najlepiej umieszczając składniki w małym słoiczku, zakręcić zakrętkę i energicznie potrząsnąć kilka razy." +
-                "2. Na zieleninie rozłożyć przygotowane składniki, posypać grzankami i czarnuszką oraz rozdrobnionym serem, polać sosem i na sam koniec dodać odrobinę szczypiorku i natki.\n" +
-                "Sałatka gotowa jest do spożycia.");
+                "Odcedź makaron i kalarepkę. Wyłóż wszystko na talerz. Posyp natką pietruszki i bazylią. Smacznego\n");
 
 
         odp = odpowiedzi.get(index);
